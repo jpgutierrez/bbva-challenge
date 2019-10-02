197abd6267699953ce8edd5afee77ffd91616db7
Juan Pablo Gutierrez <jpgutierrezalegre@gmail.com>
2019-10-01 20:29:30 -0500

    Gateway mapping

--- src/main/java/com/bbva/challenge/controller/GatewayController.java
+++ src/main/java/com/bbva/challenge/controller/GatewayController.java
@@ -9,11 +9,13 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(value = "Gateway")
 @RestController
+@RequestMapping(value = "/gateway")
 public class GatewayController {
 
     @Autowired
